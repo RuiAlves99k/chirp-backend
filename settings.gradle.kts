5d11e8ef79447987323aebbf15c3b05031cb8ac5
Rui Alves <ruialves99k@gmail.com>
2025-11-03 13:54:02 +0100

    add all modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,15 @@
 pluginManagement {
-	repositories {
-		maven { url = uri("https://repo.spring.io/snapshot") }
-		gradlePluginPortal()
-	}
+    repositories {
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
 }
 rootProject.name = "chirp"
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+include("app")
+include("user")
+include("chat")
+include("notification")
+include("common")
