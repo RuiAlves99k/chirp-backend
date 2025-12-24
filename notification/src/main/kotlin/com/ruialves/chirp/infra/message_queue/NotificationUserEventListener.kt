abcfe9b2c3453c5698bc296e318777b84c2cb826
Rui Alves <ruialves99k@gmail.com>
2025-12-24 20:30:07 +0000

    fix transactional publisher

---
+++ notification/src/main/kotlin/com/ruialves/chirp/infra/message_queue/NotificationUserEventListener.kt
@@ -0,0 +1,29 @@
+package com.ruialves.chirp.infra.message_queue
+
+import com.ruialves.chirp.domain.events.user.UserEvent
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class NotificationUserEventListener {
+
+    @RabbitListener(queues = [MessageQueues.NOTIFICATION_USER_EVENTS])
+    fun handleUserEvent(event: UserEvent) {
+        when (event) {
+            is UserEvent.Created -> {
+                println("User created!")
+            }
+
+            is UserEvent.RequestResendVerification -> {
+                println("Request resend verification")
+            }
+
+            is UserEvent.RequestResetPassword -> {
+                println("Request reset password")
+            }
+
+            else -> Unit
+        }
+    }
+}
