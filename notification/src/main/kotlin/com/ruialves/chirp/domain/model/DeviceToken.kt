f50b6c9018713c16e0ffd54f2e4385eb28e52c69
Rui Alves <ruialves99k@gmail.com>
2026-03-15 20:36:06 +0100

    setup firebase for notifications

---
+++ notification/src/main/kotlin/com/ruialves/chirp/domain/model/DeviceToken.kt
@@ -0,0 +1,16 @@
+package com.ruialves.chirp.domain.model
+
+import com.ruialves.chirp.domain.type.UserId
+import java.time.Instant
+
+data class DeviceToken(
+    val id: Long,
+    val userId: UserId,
+    val token: String,
+    val platform: Platform,
+    val createdAt: Instant = Instant.now()
+) {
+    enum class Platform {
+        ANDROID, IOS
+    }
+}
